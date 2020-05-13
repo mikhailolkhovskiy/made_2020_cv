618e064cb2be3c54f80caa0130b77ede8e7c7196
mikhailo <olhovskiyml@gmail.com>
2020-05-12 22:08:10 -0700

    add contest 1

---
+++ contest1/01_train_full_resnext101_32x8d/run.sh
@@ -0,0 +1,1 @@
+python hack_train.py --name "model2" --data ../data/data --gpu -b 64 -e 6 -lr 0.001 
---
+++ contest1/02_tain_last_fc/run.sh
@@ -0,0 +1,1 @@
+python hack_train.py --name "model3" --data ../data/data --gpu -b 64 -e 10 -lr 0.001 
